346529bd1a6d861ce12a203f48c2f73b28940029
Cesar de Paula <cdepaula942@gmail.com>
2023-04-06 02:52:44 -0700

    added index.php, script.js, style.css

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-    
+    include("header.html");
+
 ?>
+
+<html>
+    <head>
+        <title>Roommate Application</title>
+        <link rel="stylesheet" href="styles/style.css">
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            Name:<br>
+            <input type="text" name="name"><br>
+            Email:<br>
+            <input type="text" name="email"><br>
+            Bio:<br>
+            <input type="text" name="biography"><br>
+            Education:<br>
+            <input type="text" name="education"><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+
+    </body>
+</html>
